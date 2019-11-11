5c5cd411b4da2e2742b8a93dcb20337652f91167
Dr.Abc <29565652+DrAbcrealone@users.noreply.github.com>
2019-11-11 23:00:46 +0800

    123123

--- XiangShuScroll/XiangShuScroll.cs
+++ XiangShuScroll/XiangShuScroll.cs
@@ -11,9 +11,7 @@ namespace XiangShuScroll
 {
     public class Settings : UnityModManager.ModSettings
     {
-        public string name = "相枢";
-        public string swordname = "EX咖喱棒";
-        public string badname = "太吾";
+        public string[] name = { "相枢", "太吾", "EX咖喱棒" };
         public override void Save(UnityModManager.ModEntry modEntry)
         {
             Save(this, modEntry);
@@ -23,11 +21,11 @@ namespace XiangShuScroll
 
     public class Main
     {
-
-        public static bool enabled;
         public static Settings settings;
         public static UnityModManager.ModEntry.ModLogger Logger;
         public static int num = 0;
+        public static bool enabled, update = false;
+        public static List<int> nullObj = new List<int>();
 
         public static bool Load(UnityModManager.ModEntry mod)
         {
@@ -49,7 +47,7 @@ namespace XiangShuScroll
             if (!val)
                 return false;
 
-            enabled = val;
+            Main.enabled = val;
             return true;
         }
 
@@ -58,21 +56,43 @@ namespace XiangShuScroll
             GUILayout.Label("");
             GUILayout.BeginHorizontal("Box");
             GUILayout.Label("修改太吾名称:"); GUILayout.Space(16);
-            settings.name = GUILayout.TextField(settings.name, "宁叫啥勒？"); GUILayout.Space(16);
-            GUILayout.Label("修改剑柄名称:"); GUILayout.Space(16);
-            settings.swordname = GUILayout.TextField(settings.swordname, "剑叫啥勒？"); GUILayout.Space(16);
+            settings.name[0] = GUILayout.TextField(settings.name[0], "宁叫啥勒？"); GUILayout.Space(16);
             GUILayout.Label("修改相枢名称:"); GUILayout.Space(16);
-            settings.badname = GUILayout.TextField(settings.badname, "剑叫啥勒？"); GUILayout.Space(16);
+            settings.name[1] = GUILayout.TextField(settings.name[1], "他叫啥勒？"); GUILayout.Space(16);
+            GUILayout.Label("修改剑柄名称:"); GUILayout.Space(16);
+            settings.name[2] = GUILayout.TextField(settings.name[2], "剑叫啥勒？"); GUILayout.Space(16);
+
             if (GUILayout.Button("更新名称"))
-                Doit();
+                Main.update = true;
             GUILayout.Space(16);
-            GUILayout.Label("修改将在重启游戏后生效！");
+            if (Main.update)
+                GUILayout.Label("<color=#E4504DFF>修改将在重启游戏后生效！</color>");
+            else
+                GUILayout.Label("修改将在重启游戏后生效");
             GUILayout.FlexibleSpace();
             GUILayout.EndHorizontal();
         }
 
         public static void Doit()
         {
+            for (int i = 0; i < settings.name.Length; i++)
+            {
+                if (string.IsNullOrEmpty(settings.name[i]))
+                    nullObj.Add(i);
+            }
+            if (nullObj.Count != 0)
+            {
+                foreach (int i in nullObj)
+                {
+                    switch (i)
+                    {
+                        case 0: settings.name[0] = "太吾"; break;
+                        case 1: settings.name[1] = "相枢"; break;
+                        case 2: settings.name[2] = "伏虞"; break;
+                    }
+                }
+            }
+
             num = 0;
 
             DateFile.instance.massageDate = ReplaceIt(DateFile.instance.massageDate);
@@ -94,7 +114,7 @@ namespace XiangShuScroll
             DateFile.instance.baseTipsDate = ReplaceIt(DateFile.instance.baseTipsDate);
             DateFile.instance.presetitemDate = ReplaceIt(DateFile.instance.presetitemDate);
 
-            Logger.Log(string.Format("共替换了{0}个句子，游戏愉快！{1}!", num, settings.name));
+            Logger.Log(string.Format("共替换了{0}个句子，游戏愉快！{1}!", num, settings.name[0]));
 #if array
             num = 0;
             for (int i = 0; i < dictionaries.Length; i++)
@@ -106,33 +126,79 @@ namespace XiangShuScroll
             }
 #endif
         }
+        private static readonly string[] cnNumberString = { "零", "一", "二", "三", "四", "五", "六", "七", "八", "九" };
+        private static readonly string[] cnMaxUnit = { "", "万", "亿", "万亿" };
+        private static readonly string[] cnMinUnit = { "", "十", "百", "千" };
 
-        private static string SwordDetailCombiner(in string replacer)
+        private static void SectionToChines(int section, ref string cnString)
         {
-            string cache = "";
-            for (int i = 0; i < replacer.Length; i++)
+            string strIns = string.Empty;
+            int unitPos = 0;
+            bool zero = true;
+            while (section > 0)
             {
-                cache += "“" + replacer[i] + "”";
-            }
-            string len = replacer.Length.ToString();
-            for (int j = 0; j < len.Length; j++)
-            {
-                switch (len[j])
+                int v = section % 10;
+                if (v == 0)
                 {
-                    default: continue;
-                    case '1': cache += "一"; break;
-                    case '2': cache += "二"; break;
-                    case '3': cache += "三"; break;
-                    case '4': cache += "四"; break;
-                    case '5': cache += "五"; break;
-                    case '6': cache += "六"; break;
-                    case '7': cache += "七"; break;
-                    case '8': cache += "八"; break;
-                    case '9': cache += "九"; break;
-                    case '0': cache += "十"; break;
+                    if (!zero)
+                    {
+                        zero = true;
+                        cnString = cnString.Insert(0, cnNumberString[v]);
+                    }
                 }
+                else
+                {
+                    zero = false;
+                    strIns = $"{cnNumberString[v]}{cnMinUnit[unitPos]}";
+                    cnString = cnString.Insert(0, strIns);
+                }
+                unitPos++;
+                section /= 10;
             }
-            return cache;
+        }
+
+        public static string GetNumber(int number)
+        {
+            int tempNumber = Math.Abs(number);
+            if (tempNumber == 0)
+            {
+                return "零";
+            }
+
+            string result = string.Empty;
+            int unitPos = 0;
+            bool needZero = false;
+            while (tempNumber > 0)
+            {
+                string strIns = string.Empty;
+                int section = tempNumber % 10000;
+                if (needZero)
+                {
+                    result = result.Insert(0, cnNumberString[0]);
+                }
+                SectionToChines(section, ref strIns);
+                strIns += (section != 0) ? cnMaxUnit[unitPos] : cnMaxUnit[0];
+                result = result.Insert(0, strIns);
+                needZero = (section < 1000) && (section > 0);
+                tempNumber /= 10000;
+                unitPos++;
+            }
+            if (number < 0)
+            {
+                result = $"负{result}";
+            }
+            return result;
+        }
+
+        public static string SwordDetailCombiner(in string cache)
+        {
+            string res = string.Empty;
+            foreach (char c in cache)
+            {
+                res += "“" + c + "”";
+            }
+            res += GetNumber(cache.Length);
+            return res;
         }
 
         private static Dictionary<int, Dictionary<int, string>> ReplaceIt(Dictionary<int, Dictionary<int, string>> cacheData)
@@ -148,7 +214,7 @@ namespace XiangShuScroll
                         num++;
                     try
                     {
-                        a.Add(pair.Key, string.Format(pair.Value.Replace("太吾", "{0}").Replace("相枢", "{1}").Replace("伏虞", "{2}").Replace("“伏”“虞”二", "{3}"), settings.name, settings.badname, settings.swordname, SwordDetailCombiner(settings.swordname)));
+                        a.Add(pair.Key, string.Format(pair.Value.Replace("太吾", "{0}").Replace("相枢", "{1}").Replace("伏虞", "{2}").Replace("“伏”“虞”二", "{3}"), settings.name[0], settings.name[1], settings.name[2], SwordDetailCombiner(settings.name[2])));
                     }
                     catch (Exception e)
                     {
@@ -157,15 +223,14 @@ namespace XiangShuScroll
                 }
                 b.Add(value.Key, a);
             }
-            Logger.Log("未替换" + i + "项");
+            if (i != 0)
+                Logger.Log("未替换" + i + "项");
             return b;
         }
-
         public static void OnSaveGUI(UnityModManager.ModEntry mod)
         {
             settings.Save(mod);
         }
-
         [HarmonyPatch(typeof(ArchiveSystem.LoadGame), "LoadReadonlyData")]
         private static class ArchiveSystem_LoadGame_LoadReadonlyData_Patch
         {
