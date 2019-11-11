2c96c95484fa9c32ad7d987bdfd8da113f0f98ea
Dr.Abc <29565652+DrAbcrealone@users.noreply.github.com>
2019-11-11 23:52:01 +0800

    加注释

--- XiangShuScroll/XiangShuScroll.cs
+++ XiangShuScroll/XiangShuScroll.cs
@@ -11,6 +11,9 @@ namespace XiangShuScroll
 {
     public class Settings : UnityModManager.ModSettings
     {
+        /// <summary>
+        /// 替换内容 太吾名称 相枢名称 神剑名称
+        /// </summary>
         public string[] name = { "相枢", "太吾", "EX咖喱棒" };
         public override void Save(UnityModManager.ModEntry modEntry)
         {
@@ -23,10 +26,31 @@ namespace XiangShuScroll
     {
         public static Settings settings;
         public static UnityModManager.ModEntry.ModLogger Logger;
-        public static int num = 0;
+        /// <summary>
+        /// 替换条目统计
+        /// </summary>
+        private static int num = 0;
+        /// <summary>
+        /// 是否启动，是否更新
+        /// </summary>
         public static bool enabled, update = false;
-        public static List<int> nullObj = new List<int>();
-
+        /// <summary>
+        /// 空白项
+        /// </summary>
+        private static readonly string[] cnNumberString = { "零", "一", "二", "三", "四", "五", "六", "七", "八", "九" };
+        /// <summary>
+        /// 大单位替换中文字符
+        /// </summary>
+        private static readonly string[] cnMinUnit = { "", "十", "百", "千" };
+        /// <summary>
+        /// 特大单位替换中文字符，你写这么长的剑柄名是干什么
+        /// </summary>
+        private static readonly string[] cnMaxUnit = { "", "万", "亿", "万亿" };
+        /// <summary>
+        /// 入口点
+        /// </summary>
+        /// <param name="mod">mod</param>
+        /// <returns></returns>
         public static bool Load(UnityModManager.ModEntry mod)
         {
             Logger = mod.Logger;
@@ -41,7 +65,12 @@ namespace XiangShuScroll
             mod.OnSaveGUI = OnSaveGUI;
             return true;
         }
-
+        /// <summary>
+        /// 切换
+        /// </summary>
+        /// <param name="mod">控制的mod</param>
+        /// <param name="val">是否启用</param>
+        /// <returns></returns>
         public static bool OnToggle(UnityModManager.ModEntry mod, bool val)
         {
             if (!val)
@@ -50,10 +79,12 @@ namespace XiangShuScroll
             Main.enabled = val;
             return true;
         }
-
-        public static void OnGUI(UnityModManager.ModEntry mody)
+        /// <summary>
+        /// GUI界面
+        /// </summary>
+        /// <param name="mod">控制的mod</param>
+        public static void OnGUI(UnityModManager.ModEntry mod)
         {
-            GUILayout.Label("");
             GUILayout.BeginHorizontal("Box");
             GUILayout.Label("修改太吾名称:"); GUILayout.Space(16);
             settings.name[0] = GUILayout.TextField(settings.name[0], "宁叫啥勒？"); GUILayout.Space(16);
@@ -72,64 +103,46 @@ namespace XiangShuScroll
             GUILayout.FlexibleSpace();
             GUILayout.EndHorizontal();
         }
-
+        public static void OnSaveGUI(UnityModManager.ModEntry mod)
+        {
+            settings.Save(mod);
+        }
+        /// <summary>
+        /// 开始了
+        /// </summary>
         public static void Doit()
         {
-            for (int i = 0; i < settings.name.Length; i++)
-            {
-                if (string.IsNullOrEmpty(settings.name[i]))
-                    nullObj.Add(i);
-            }
-            if (nullObj.Count != 0)
-            {
-                foreach (int i in nullObj)
-                {
-                    switch (i)
-                    {
-                        case 0: settings.name[0] = "太吾"; break;
-                        case 1: settings.name[1] = "相枢"; break;
-                        case 2: settings.name[2] = "伏虞"; break;
-                    }
-                }
-            }
-
+            //归零计数
             num = 0;
 
-            DateFile.instance.massageDate = ReplaceIt(DateFile.instance.massageDate);
-            DateFile.instance.trunEventDate = ReplaceIt(DateFile.instance.trunEventDate);
-            DateFile.instance.actorMassageDate = ReplaceIt(DateFile.instance.actorMassageDate);
-            DateFile.instance.placeWorldDate = ReplaceIt(DateFile.instance.placeWorldDate);
-            DateFile.instance.resourceDate = ReplaceIt(DateFile.instance.resourceDate);
-            DateFile.instance.presetGangDate = ReplaceIt(DateFile.instance.presetGangDate);
-            DateFile.instance.presetGangGroupDateValue = ReplaceIt(DateFile.instance.presetGangGroupDateValue);
-            DateFile.instance.actorFeaturesDate = ReplaceIt(DateFile.instance.actorFeaturesDate);
-            DateFile.instance.baseStoryDate = ReplaceIt(DateFile.instance.baseStoryDate);
-            DateFile.instance.eventDate = ReplaceIt(DateFile.instance.eventDate);
-            DateFile.instance.identityDate = ReplaceIt(DateFile.instance.identityDate);
-            DateFile.instance.tipsMassageDate = ReplaceIt(DateFile.instance.tipsMassageDate);
-            DateFile.instance.baseMissionDate = ReplaceIt(DateFile.instance.baseMissionDate);
-            DateFile.instance.scoreBootyDate = ReplaceIt(DateFile.instance.scoreBootyDate);
-            DateFile.instance.legendBookEffectData = ReplaceIt(DateFile.instance.legendBookEffectData);
-            DateFile.instance.basehomePlaceDate = ReplaceIt(DateFile.instance.basehomePlaceDate);
-            DateFile.instance.baseTipsDate = ReplaceIt(DateFile.instance.baseTipsDate);
-            DateFile.instance.presetitemDate = ReplaceIt(DateFile.instance.presetitemDate);
+            //怎么保存地址到一个数组里呢.......
+            ReplaceIt(ref DateFile.instance.massageDate);
+            ReplaceIt(ref DateFile.instance.trunEventDate);
+            ReplaceIt(ref DateFile.instance.actorMassageDate);
+            ReplaceIt(ref DateFile.instance.placeWorldDate);
+            ReplaceIt(ref DateFile.instance.resourceDate);
+            ReplaceIt(ref DateFile.instance.presetGangDate);
+            ReplaceIt(ref DateFile.instance.presetGangGroupDateValue);
+            ReplaceIt(ref DateFile.instance.actorFeaturesDate);
+            ReplaceIt(ref DateFile.instance.baseStoryDate);
+            ReplaceIt(ref DateFile.instance.eventDate);
+            ReplaceIt(ref DateFile.instance.identityDate);
+            ReplaceIt(ref DateFile.instance.tipsMassageDate);
+            ReplaceIt(ref DateFile.instance.baseMissionDate);
+            ReplaceIt(ref DateFile.instance.scoreBootyDate);
+            ReplaceIt(ref DateFile.instance.legendBookEffectData);
+            ReplaceIt(ref DateFile.instance.basehomePlaceDate);
+            ReplaceIt(ref DateFile.instance.baseTipsDate);
+            ReplaceIt(ref DateFile.instance.presetitemDate);
 
             Logger.Log(string.Format("共替换了{0}个句子，游戏愉快！{1}!", num, settings.name[0]));
-#if array
-            num = 0;
-            for (int i = 0; i < dictionaries.Length; i++)
-            {
-                if (dictionaries[i] is null)
-                    Logger.Log("数据编号:" + i + "为空!已跳过....");
-                else
-                    ReplaceIt(dictionaries[i]);
-            }
-#endif
         }
-        private static readonly string[] cnNumberString = { "零", "一", "二", "三", "四", "五", "六", "七", "八", "九" };
-        private static readonly string[] cnMaxUnit = { "", "万", "亿", "万亿" };
-        private static readonly string[] cnMinUnit = { "", "十", "百", "千" };
 
+        /// <summary>
+        /// 小数目替换
+        /// </summary>
+        /// <param name="section">片段</param>
+        /// <param name="cnString">中文字符</param>
         private static void SectionToChines(int section, ref string cnString)
         {
             string strIns = string.Empty;
@@ -156,7 +169,11 @@ namespace XiangShuScroll
                 section /= 10;
             }
         }
-
+        /// <summary>
+        /// 替换为中文数字
+        /// </summary>
+        /// <param name="number">数字</param>
+        /// <returns></returns>
         public static string GetNumber(int number)
         {
             int tempNumber = Math.Abs(number);
@@ -189,7 +206,11 @@ namespace XiangShuScroll
             }
             return result;
         }
-
+        /// <summary>
+        /// 剑柄描述生成
+        /// </summary>
+        /// <param name="cache">剑柄名</param>
+        /// <returns></returns>
         public static string SwordDetailCombiner(in string cache)
         {
             string res = string.Empty;
@@ -200,8 +221,12 @@ namespace XiangShuScroll
             res += GetNumber(cache.Length);
             return res;
         }
-
-        private static Dictionary<int, Dictionary<int, string>> ReplaceIt(Dictionary<int, Dictionary<int, string>> cacheData)
+        /// <summary>
+        /// 替换部分
+        /// </summary>
+        /// <param name="cacheData">需要修改的内容</param>
+        /// <returns></returns>
+        private static void ReplaceIt(ref Dictionary<int, Dictionary<int, string>> cacheData)
         {
             int i = 0;
             Dictionary<int, Dictionary<int, string>> b = new Dictionary<int, Dictionary<int, string>>();
@@ -210,14 +235,32 @@ namespace XiangShuScroll
                 Dictionary<int, string> a = new Dictionary<int, string>();
                 foreach (KeyValuePair<int, string> pair in value.Value)
                 {
-                    if (pair.Value.Contains("太吾"))
+                    //计数加一√
+                    if (pair.Value.Contains("太吾") || pair.Value.Contains("相枢") || pair.Value.Contains("伏虞"))
                         num++;
+
+                    //替换内容为C#占位符，如果为空则不替换
+                    string cache = pair.Value;
+                    if (!string.IsNullOrEmpty(settings.name[0]))
+                        cache = cache.Replace("太吾", "{0}");
+                    if (!string.IsNullOrEmpty(settings.name[1]))
+                        cache = cache.Replace("相枢", "{1}");
+                    if (!string.IsNullOrEmpty(settings.name[2]))
+                    {
+                        cache = cache.Replace("伏虞", "{2}");
+                        cache = cache.Replace("“伏”“虞”二", "{3}");
+                    }
+
                     try
                     {
-                        a.Add(pair.Key, string.Format(pair.Value.Replace("太吾", "{0}").Replace("相枢", "{1}").Replace("伏虞", "{2}").Replace("“伏”“虞”二", "{3}"), settings.name[0], settings.name[1], settings.name[2], SwordDetailCombiner(settings.name[2])));
+                        //把占位符换为输入内容
+                        cache = string.Format(cache, settings.name[0], settings.name[1], settings.name[2], SwordDetailCombiner(settings.name[2]));
+
+                        a.Add(pair.Key, cache);
                     }
                     catch (Exception e)
                     {
+                        //出错未替换项目就throw了，然后+1
                         i++;
                     }
                 }
@@ -225,12 +268,11 @@ namespace XiangShuScroll
             }
             if (i != 0)
                 Logger.Log("未替换" + i + "项");
-            return b;
+            cacheData = b;
         }
-        public static void OnSaveGUI(UnityModManager.ModEntry mod)
-        {
-            settings.Save(mod);
-        }
+        /// <summary>
+        /// 读取完默认资源后开始替换
+        /// </summary>
         [HarmonyPatch(typeof(ArchiveSystem.LoadGame), "LoadReadonlyData")]
         private static class ArchiveSystem_LoadGame_LoadReadonlyData_Patch
         {
